7326238b519065e389807cbc2adbe7bcae1b7c13
jinsol0330 <jinsol0330@naver.com>
2022-03-13 15:12:20 +0900

    Initial setting

---
+++ src/main/java/com/codepresso/meu/MeuApplication.java
@@ -0,0 +1,13 @@
+package com.codepresso.meu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codepresso/meu/MeuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codepresso.meu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
