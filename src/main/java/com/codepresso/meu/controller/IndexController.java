fbb15d5161e887ff9133d6f95e02080a7c612914
HanJaemin-kr <dudrn1947@gmail.com>
2022-03-13 22:32:18 +0900

    first commit

---
+++ src/main/java/com/codepresso/meu/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.codepresso.meu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    public IndexController() {
+
+    };
+
+    @RequestMapping(value="/")
+    public String index() {
+        return "index";
+    }
+
+}
